8289948f6382d0d5a935dbcacf7f09f5770855e3
Rohitmarwarpay <rohit.marwarpay@gmail.com>
2024-12-04 14:18:14 +0530

    FINAL COMMIT

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+const MONGO_URI = "mongodb+srv://mainuser:test@cluster0.u7vt3.mongodb.net/";
+// const MONGO_URI = "mongodb://localhost:27017/e-commerce"
+
+export const DBConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error connecting to database:", error.message);
+  }
+};
---
+++ src/routes/product.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createProduct, deleteProduct, getProduct, getProducts } from "../controller/Product.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const productRoutes = express.Router();
+
+productRoutes.post("/createproduct", verifyToken, createProduct);
+productRoutes.get("/getproducts",verifyToken, getProducts);
+productRoutes.get("/getproduct/:id",verifyToken, getProduct);
+productRoutes.delete("/delete/:id",verifyToken, deleteProduct);
+
+export default productRoutes;
